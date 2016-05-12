fbfe4c7fd1d2d0d6bcad6ee4f68d9dc19dac8c7a
William Constantine <billc@zillow.com>
2016-05-12 11:06:53 -0700

    Initial entry

---
+++ src/peaks.f
@@ -0,0 +1,42 @@
+c %Z%%Q%
+	subroutine splus2Rpeaks(x,halfspan,strict,n,m,out)
+C uncomment next line for verifying everything is declared
+C     implicit undefined (a-z)
+C out and o logically should be logicals, but would have to
+C change Splus function peaks and what else to reflect that.
+        integer n, m
+	real*8 x(n,m),out(n,m)
+	integer halfspan
+	logical strict
+
+        real*8 t, o
+        integer i, j, k, ik
+
+	do 200 j=1,m
+	do 200 i=1,n
+		t=x(i,j)
+		o=1.0
+		do 100 k=-halfspan,halfspan
+			ik=i+k
+			if(ik.le.0.or.ik.gt.n) then
+				o=0.0
+				go to 200
+			else
+				if(k.eq.0)go to 100
+				if (strict) then
+					if (t.le.x(ik,j)) then
+					o=0
+					go to 200	
+					endif
+				else
+					if (t.lt.x(ik,j)) then
+					o=0
+					go to 200
+					endif
+				endif
+			endif
+100		continue
+		out(i,j)=o
+200	continue
+	return
+	end
